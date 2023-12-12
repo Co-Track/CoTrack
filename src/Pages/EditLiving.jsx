d46b97e2f39a531054c46452ce51795370e625ca
Menatalla Gomaa <minafathi98@gmail.com>
2023-12-12 20:36:12 +0100

    edit in the living component-DONE

--- src/Pages/EditLiving.jsx
+++ src/Pages/EditLiving.jsx
@@ -1,6 +1,21 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import axios from "axios";
-import { useNavigate } from "react-router-dom";
+import { useNavigate, useParams } from "react-router-dom";
+
+function formatDateString(inputDateString) {
+  // Create a Date object from the input string
+  let inputDate = new Date(inputDateString);
+
+  // Get the components of the date
+  let year = inputDate.getUTCFullYear();
+  let month = ("0" + (inputDate.getUTCMonth() + 1)).slice(-2);
+  let day = ("0" + inputDate.getUTCDate()).slice(-2);
+
+  // Format the date as "yyyy-MM-dd"
+  let formattedDate = year + "-" + month + "-" + day;
+
+  return formattedDate;
+}
 
 function EditLiving() {
   const [income, setIncome] = useState("");
@@ -8,28 +23,49 @@ function EditLiving() {
   const [inDate, setInDate] = useState("");
   const [title, setTitle] = useState("");
 
-  const navigate = useNavigate();
+  const { livingId } = useParams();
+  console.log(livingId);
 
-  const handleSubmit = (e) => {
-    e.preventDefault();
-    const requestBody = {
-      income: income,
-      outcome: outcome,
-      inDate: inDate,
-      title: title,
-    };
-    const storedToken = localStorage.getItem("authToken");
+  const navigate = useNavigate();
+  const storedToken = localStorage.getItem("authToken");
+
+  useEffect(() => {
     axios
-      .put("http://localhost:3000/living/:livingId", requestBody, {
+      .get(`http://localhost:3000/living/${livingId}`, {
         headers: { Authorization: `Bearer ${storedToken}` },
+      })
+      .then((response) => {
+        console.log(response.data);
+        setTitle(response.data.title);
+        setIncome(response.data.income);
+        setInDate(formatDateString(response.data.inDate));
+      });
+  }, []);
+
+  const handleFormSubmit = (e) => {
+    e.preventDefault();
+
+    console.log(storedToken);
+    const formData = {
+      title,
+      inCome: income,
+      inDate: inDate,
+      outCome: outcome,
+    };
+    axios
+      .put("http://localhost:3000/living/" + livingId, formData, {
+        headers: {
+          Authorization: `Bearer ${localStorage.getItem("authToken")}`,
+        },
       })
       .then((response) => {
         setIncome("");
         setOutcome("");
         setInDate("");
         setTitle("");
-        navigate("/living");
         console.log(response);
+
+        navigate("/living");
       })
       .catch((error) => {
         console.log(error);
@@ -38,7 +74,7 @@ function EditLiving() {
 
   return (
     <div className="inputs-container">
-      <form onSubmit={handleSubmit}>
+      <form onSubmit={handleFormSubmit}>
         <div>
           <h1>Edit an existing expense</h1>
           <label>Title</label>
@@ -48,8 +84,8 @@ function EditLiving() {
             placeholder="enter the title"
             value={title}
             required={true}
-            onChange={(e) => {
-              setTitle(e.target.value);
+            onChange={(id) => {
+              setTitle(id.target.value);
             }}
           />
         </div>
@@ -60,8 +96,8 @@ function EditLiving() {
             type="number"
             placeholder="00.00"
             value={income}
-            onChange={(e) => {
-              setIncome(e.target.value);
+            onChange={(id) => {
+              setIncome(id.target.value);
             }}
           />
         </div>
@@ -71,8 +107,8 @@ function EditLiving() {
             className="inputs"
             type="number"
             value={outcome}
-            onChange={(e) => {
-              setOutcome(e.target.value);
+            onChange={(id) => {
+              setOutcome(id.target.value);
             }}
           />
         </div>
@@ -82,14 +118,16 @@ function EditLiving() {
             className="inputs"
             type="date"
             value={inDate}
-            onChange={(e) => {
-              setInDate(e.target.value);
+            onChange={(id) => {
+              setInDate(id.target.value);
             }}
           />
         </div>
         <div>
           <br></br>
-          <button className="addBtn">Edit</button>
+          <button type="submit" className="addBtn">
+            Edit
+          </button>
         </div>
       </form>
     </div>
