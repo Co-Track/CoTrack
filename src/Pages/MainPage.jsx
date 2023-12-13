baf6f5933617208ee3d661fd7e3344f5d8fd6d9f
Menatalla Gomaa <minafathi98@gmail.com>
2023-12-13 17:28:28 +0100

    FAQ section- DONE

--- src/Pages/MainPage.jsx
+++ src/Pages/MainPage.jsx
@@ -14,9 +14,7 @@ import FAQ from "../components/FAQ";
 const MainPage = () => {
   return (
     <div>
-      <div className="main-title">
-        <h1 className="p-name">CoTrack</h1>
-      </div>
+      <div className="main-title"></div>
       <FAQ />
     </div>
   );
--- src/components/FAQ.jsx
+++ src/components/FAQ.jsx
@@ -89,123 +89,142 @@ import "./FAQ.css";
 
 const FAQ = () => {
   return (
-    <div className="faq_area section_padding_130" id="faq">
-      <div className="container">
-        <div className="row justify-content-center">
-          <div className="col-12 col-sm-8 col-lg-6">
-            <div
-              className="section_heading text-center wow fadeInUp"
-              data-wow-delay="0.2s"
-            >
-              <h3>
-                <span>Frequently </span> Asked Questions
-              </h3>
-
-              <div className="line"></div>
+    <>
+      <div className="faq_area section_padding_130" id="faq">
+        <h3>
+          <span> FAQ </span>
+        </h3>
+        <div className="container-fluid">
+          <div className="row justify-content-center">
+            <div className="col-12 col-sm-8 col-lg-6">
+              <div
+                className="section_heading text-center wow fadeInUp"
+                data-wow-delay="0.2s"
+              >
+                <div className="line"></div>
+              </div>
             </div>
           </div>
-        </div>
-        <div className="row justify-content-center">
-          <div className="col-12 col-sm-10 col-lg-8">
-            <div className="accordion faq-accordian" id="faqAccordion">
-              {/* Question 1 */}
-              <div className="card border-0 wow fadeInUp" data-wow-delay="0.2s">
-                <div className="card-header" id="headingOne">
-                  <h6
-                    className="mb-0 collapsed"
-                    data-toggle="collapse"
-                    data-target="#collapseOne"
-                    aria-expanded="true"
-                    aria-controls="collapseOne"
-                  >
-                    What is the purpose of this expense tracker app?{" "}
-                    <span className="lni-chevron-up"></span>
-                  </h6>
-                </div>
+
+          <div className="row justify-content-center">
+            {/* Update the container class to use 'container-fluid' */}
+            <div className="col-12 col-sm-10 col-lg-8">
+              <div className="accordion faq-accordian" id="faqAccordion">
+                {/* Question 1 */}
                 <div
-                  className="collapse"
-                  id="collapseOne"
-                  aria-labelledby="headingOne"
-                  data-parent="#faqAccordion"
+                  className="card border-0 wow fadeInUp"
+                  data-wow-delay="0.2s"
                 >
-                  <div className="card-body">
-                    <p>
-                      The expense tracker app helps you keep track of your
-                      personal, living, and emergency expenses. It provides a
-                      convenient way to monitor your spending, categorize
-                      expenses, and maintain a budget.
-                    </p>
+                  <div className="card-header" id="headingOne">
+                    <h6
+                      className="mb-0 collapsed"
+                      data-toggle="collapse"
+                      data-target="#collapseOne"
+                      aria-expanded="true"
+                      aria-controls="collapseOne"
+                    >
+                      What is the purpose of this expense tracker app?{" "}
+                      <span className="lni-chevron-up"></span>
+                    </h6>
+                  </div>
+                  <div
+                    className="collapse"
+                    id="collapseOne"
+                    aria-labelledby="headingOne"
+                    data-parent="#faqAccordion"
+                  >
+                    <div className="card-body">
+                      <p>
+                        The expense tracker app helps you keep track of your
+                        personal, living, and emergency expenses. It provides a
+                        convenient way to monitor your spending, categorize
+                        expenses, and maintain a budget.
+                      </p>
+                    </div>
+                  </div>
+                </div>
+                {/* Question 2 */}
+                <div
+                  className="card border-0 wow fadeInUp"
+                  data-wow-delay="0.3s"
+                >
+                  <div className="card-header" id="headingTwo">
+                    <h6
+                      className="mb-0 collapsed"
+                      data-toggle="collapse"
+                      data-target="#collapseTwo"
+                      aria-expanded="true"
+                      aria-controls="collapseTwo"
+                    >
+                      How do I add an expense in the app?{" "}
+                      <span className="lni-chevron-up"></span>
+                    </h6>
+                  </div>
+                  <div
+                    className="collapse"
+                    id="collapseTwo"
+                    aria-labelledby="headingTwo"
+                    data-parent="#faqAccordion"
+                  >
+                    <div className="card-body">
+                      <p>
+                        To add an expense, navigate to the "Add Expense" section
+                        and enter the necessary details such as the amount,
+                        category, and the date. Click the "Add" button to record
+                        the expense.{" "}
+                      </p>
+                    </div>
+                  </div>
+                </div>
+
+                {/* Question 3 */}
+                <div
+                  className="card border-0 wow fadeInUp"
+                  data-wow-delay="0.4s"
+                >
+                  <div className="card-header" id="headingThree">
+                    <h6
+                      className="mb-0 collapsed"
+                      data-toggle="collapse"
+                      data-target="#collapseThree"
+                      aria-expanded="true"
+                      aria-controls="collapseThree"
+                    >
+                      Can I use the app on multiple devices?{" "}
+                      <span className="lni-chevron-up"></span>
+                    </h6>
+                  </div>
+                  <div
+                    className="collapse"
+                    id="collapseThree"
+                    aria-labelledby="headingThree"
+                    data-parent="#faqAccordion"
+                  >
+                    <div className="card-body">
+                      <p>
+                        Yes, the app supports multiple devices. Simply log in
+                        with your account credentials, and your expense data
+                        will be synced across all your devices.{" "}
+                      </p>
+                    </div>
                   </div>
                 </div>
               </div>
 
-              {/* Question 2 */}
-              <div className="card border-0 wow fadeInUp" data-wow-delay="0.3s">
-                <div className="card-header" id="headingTwo">
-                  <h6
-                    className="mb-0 collapsed"
-                    data-toggle="collapse"
-                    data-target="#collapseTwo"
-                    aria-expanded="true"
-                    aria-controls="collapseTwo"
-                  >
-                    The app isn't installing?
-                    <span className="lni-chevron-up"></span>
-                  </h6>
-                </div>
-                <div
-                  className="collapse"
-                  id="collapseTwo"
-                  aria-labelledby="headingTwo"
-                  data-parent="#faqAccordion"
-                >
-                  <div className="card-body">
-                    <p>
-                      Your custom content for the second question goes here.
-                    </p>
-                  </div>
-                </div>
+              {/* Support Button */}
+              <div
+                className="support-button text-center d-flex align-items-center justify-content-center mt-4 wow fadeInUp"
+                data-wow-delay="0.5s"
+              >
+                <i className="lni-emoji-sad"></i>
+                <p className="mb-0 px-2">Can't find your answers?</p>
+                <a href="#"> Contact us</a>
               </div>
-
-              {/* Question 3 */}
-              <div className="card border-0 wow fadeInUp" data-wow-delay="0.4s">
-                <div className="card-header" id="headingThree">
-                  <h6
-                    className="mb-0 collapsed"
-                    data-toggle="collapse"
-                    data-target="#collapseThree"
-                    aria-expanded="true"
-                    aria-controls="collapseThree"
-                  >
-                    Contact form isn't working?
-                    <span className="lni-chevron-up"></span>
-                  </h6>
-                </div>
-                <div
-                  className="collapse"
-                  id="collapseThree"
-                  aria-labelledby="headingThree"
-                  data-parent="#faqAccordion"
-                >
-                  <div className="card-body">
-                    <p>Your custom content for the third question goes here.</p>
-                  </div>
-                </div>
-              </div>
-            </div>
-            {/* Support Button */}
-            <div
-              className="support-button text-center d-flex align-items-center justify-content-center mt-4 wow fadeInUp"
-              data-wow-delay="0.5s"
-            >
-              <i className="lni-emoji-sad"></i>
-              <p className="mb-0 px-2">Can't find your answers?</p>
-              <a href="#"> Contact us</a>
             </div>
           </div>
         </div>
       </div>
-    </div>
+    </>
   );
 };
 
