20d58c1a6da3cac42b5e24356aa440417ffe36fb
Menatalla Gomaa <minafathi98@gmail.com>
2023-12-14 16:49:27 +0100

    Search bar - DONE

--- src/App.jsx
+++ src/App.jsx
@@ -17,11 +17,31 @@ import About from "./components/About";
 import HomePage from "./Pages/HomePage";
 import FAQ from "./components/FAQ";
 import "./App.css";
+import reactRouterToArray from "react-router-to-array";
 
 function App() {
+  const routes = reactRouterToArray(
+    <>
+      <Route path="/" element={<HomePage />} />
+      <Route path="/MainPage" element={<MainPage />} />
+      <Route path="/Living" element={<Living />} />
+      <Route path="/Personal" element={<Personal />} />
+      <Route path="/Emergency" element={<Emergency />} />
+      <Route path="/Signup" element={<Signup />} />
+      <Route path="/Login" element={<Login />} />
+      <Route path="/AddPersonal" element={<AddPersonal />} />
+      <Route path="/AddEmergency" element={<AddEmergency />} />
+      <Route path="/addLiving" element={<AddLiving />} />
+      <Route path="/EditEmergency/:emergencyId" element={<EditEmergency />} />
+      <Route path="/editLiving/:livingId" element={<EditLiving />} />
+      <Route path="/editPersonal/:personalId" element={<EditPersonal />} />
+      <Route path="/About" element={<About />} />
+      <Route path="/FAQ" element={<FAQ />} />
+    </>
+  );
   return (
     <>
-      <Navbar />
+      <Navbar routes={routes} />
       <Routes>
         <Route path="/" element={<HomePage />} />
         <Route path="/MainPage" element={<MainPage />} />
@@ -38,6 +58,10 @@ function App() {
         <Route path="/editPersonal/:personalId" element={<EditPersonal />} />
         <Route path="/About" element={<About />} />
         <Route path="/FAQ" element={<FAQ />} />
+        {/* <Route
+          path="/SearchResult"
+          element={<SearchResult routes={routes} />}
+        /> */}
       </Routes>
       <Footer />
     </>
